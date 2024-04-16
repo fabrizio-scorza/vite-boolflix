810bdb83ba01124dc2102f08f2d17af89852dcda
fabrizio-scorza <fabrizio.a.scorza@gmail.com>
2024-04-16 17:41:12 +0200

    FEAT - MILESTONE 3

--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -1,35 +1,23 @@
 <script>
 import { store } from '../../store'
+import ItemCard from './ItemCard.vue'
+
 export default {
+    components: {
+        ItemCard,
+    },
     data() {
         return {
             store,
-            lang: ['en', 'it', 'ja']
         }
-    },
+    }
 }
 </script>
 
 <template>
     <!-- <pre>{{ store.db }}</pre> -->
     <ul>
-        <li v-for="movie in store.db" :key="movie.id">
-            <h2>
-                {{ movie.title || movie.name }}
-            </h2>
-            <h4>
-                {{ movie.original_title || movie.original_name }}
-            </h4>
-            <p v-if="lang.includes(movie.original_language)">
-                <img :src="`/${movie.original_language}.png`" alt="">
-            </p>
-            <p v-else>
-                {{ movie.original_language }}
-            </p>
-            <p>
-                {{ movie.vote_average }}
-            </p>
-        </li>
+        <ItemCard v-for="movie in store.db" :key="movie.id" :item="movie" />
     </ul>
 </template>
 
--- src/components/ItemCard.vue
+++ src/components/ItemCard.vue
@@ -1,13 +1,52 @@
 <script>
-    export default {
-        
+export default {
+    props: ['item'],
+    data() {
+        return {
+            lang: ['en', 'it', 'ja'],
+        }
+    },
+    methods: {
+        starCalc(vote) {
+            let starString = '';
+            vote = Math.ceil(vote / 2);
+            let emptyVote = 5 - vote;
+
+            for (let i = 0; i < vote; i++) {
+                starString += '<i class="fa-solid fa-star"></i>'
+            }
+            for (let i = 0; i < emptyVote; i++) {
+                starString += '<i class="fa-regular fa-star"></i>'
+            }
+            return vote, emptyVote, starString;
+        }
+
     }
+}
 </script>
 
 <template>
-
+    <li class="card">
+        <div class="card-head">
+            <img :src="`https://image.tmdb.org/t/p/w342/${item.poster_path}`" alt="">
+        </div>
+        <div class="card-body">
+            <h2>
+                {{ item.title || item.name }}
+            </h2>
+            <h4>
+                {{ item.original_title || item.original_name }}
+            </h4>
+            <p v-if="lang.includes(item.original_language)">
+                <img :src="`/${item.original_language}.png`" alt="">
+            </p>
+            <p v-else>
+                {{ item.original_language }}
+            </p>
+            <p v-html="starCalc(item.vote_average)">
+            </p>
+        </div>
+    </li>
 </template>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
